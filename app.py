f426d9e8b3493d7cd6c68d757a8350d4a0bc6b49
smtnhacker <themikosurara@gmail.com>
2023-10-08 15:41:08 +0800

    feat: add sns notification

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, WebDriverException
 import boto3
 
+sns = boto3.client('sns')
 s3 = boto3.client('s3')
 bucket = 'scraped-sites-coding-test'
 
@@ -103,6 +104,9 @@ def lambda_handler(event, context):
         filename = f'{url}-{str(datetime.datetime.now())}.json'
         uploadByteStream = bytes(json.dumps(res).encode('utf-8'))
         s3.put_object(Bucket=bucket, Key=filename, Body=uploadByteStream)
+
+        sns.publish(TopicArc='arn:aws:sns:ap-southeast-1:910774986370:CloudWatch_Empty_SQS',
+                       message=f'Finished scraping {url}: {datetime.datetime.now()}')
 
         return {
             "status": 200,
